66902518330365c8a3d4efa57a7c2a3aec218eda
Eugene1x <roycekinchloe@gmail.com>
2025-07-16 06:34:33 -0400

    Setting all of my segments to 0 using xor. Setting the stack pointer and disable and then reenable interrupts

---
+++ main.asm
@@ -0,0 +1,31 @@
+16 bits
+org 0x7c00
+
+
+cli
+xor ax, ax
+mov ds, ax
+mov ss, ax
+mov es, ax
+mov sp, 0x7BFF
+sti
+
+
+mov si, 0
+
+print:
+    mov ah, 0x0e
+    mov al, [Hello + si]
+    int 0x10
+    add si, 1
+    cmp [Hello + si], 0
+    jne print
+
+jmp $
+
+Hello:
+    db "Royce's 2nd Bootloader", 0
+
+
+times 510 -($-$$) db 0
+dw 0xAA55
